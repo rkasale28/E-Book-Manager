5f222fc34f1bac564e0d0e32d2d75fd99636a063
rkasale28 <rohitkasale28@gmail.com>
2020-06-05 11:30:40 +0530

    Created User and Role models

--- manager/__init__.py
+++ manager/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 from flask_uploads import configure_uploads, IMAGES, UploadSet
+from flask_security import Security
 
-from manager.database import db,migrate
+from manager.database import db,migrate,user_datastore
 
 from manager.book.views import mod as book,covers
 from manager.student.views import mod as student,dps
@@ -17,6 +18,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/manager'
     app.config['UPLOADED_COVERS_DEST']='manager/book/static/uploads/covers'
     app.config['UPLOADED_DPS_DEST']='manager/student/static/uploads/dps'
+    app.config['USER_ENABLED_EMAIL']=False
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -30,3 +32,5 @@ manager.add_command('db', MigrateCommand)
 
 configure_uploads(app,covers)
 configure_uploads(app,dps)
+
+security = Security(app, user_datastore)
--- manager/database.py
+++ manager/database.py
@@ -1,5 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_security import SQLAlchemyUserDatastore, UserMixin, RoleMixin
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    users=db.relationship('User',backref='role')
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
